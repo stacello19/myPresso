f8fa7928884ab42db35b40eb5a8a70cc30f1e5ed
Stacy Cho <stacello19@gmail.com>
2019-06-19 23:04:11 -0400

    start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Coffees from './views/coffees';
+import TopFive from './views/top5';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <TopFive/>
+        <Coffees/>
     </div>
   );
 }
---
+++ src/views/top5.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TopFive = () => {
+    return (
+        <div>
+            <h1>these are top 5</h1>
+        </div>
+    ) 
+};
+
+export default TopFive;
