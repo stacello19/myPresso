3a4639b4450c14aa69bc30c568a586d88bcc2227
Stacy <stacello19@gmail.com>
2020-01-05 21:14:42 -0500

    database update

--- src/db/database.js
+++ src/db/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const secret = require('./secret');
-//dbname, id, pasword are all from pgAdmin (postgresql)
+
 const db = new Sequelize(secret.dbname, secret.id, secret.password, {
   host: secret.host,
   port: 5432,
@@ -8,24 +8,5 @@ const db = new Sequelize(secret.dbname, secret.id, secret.password, {
   maxConcurrentQuries: 1024,
   logging: false
 })
-
-// exports.handler = async (event, context, callback) => {
-
-//   context.callbackWaitsForEmptyEventLoop = false;
-//   db.authenticate()
-//   .then(() => {
-//     callback(null, 'Connection has been established successfully.');
-//   })
-//   .catch(err => {
-//     callback(err);
-//   });
-// }
-
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000
-//   }
 
 module.exports = db;
