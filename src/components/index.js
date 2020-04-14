9e1db34258fc023da71789052df4eb7440242473
Stacy <stacello19@gmail.com>
2020-04-14 15:15:43 -0400

    update123

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export {default as NavTop} from './NavTop/NavTop';
-export {default as Coffee} from './Coffee/Coffee';
-export {default as TopChoice} from './TopChoice/TopChoice';
-export {default as CoffeeDiary} from './CoffeeDiary/CoffeeDiary';
