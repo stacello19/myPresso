34d2796785ad53771b0c122b5aa1c50ad24e116e
Stacy <stacello19@gmail.com>
2020-04-14 15:20:36 -0400

    ignore 123

--- src/components/shared/index.js
+++
@@ -1,6 +0,0 @@
-export {default as Decaffe} from './decaffe';
-export {default as Espresso} from './espresso';
-export {default as Exclusives} from './exclusives';
-export {default as Flavored} from './flavored';
-export {default as Lungo} from './lungo';
-export {default as Mastercrafted} from './masterCrafted';
